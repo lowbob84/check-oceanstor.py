1e900c655ab440eb5b07c82ed19cd1fe3eef3688
Jurij Vasiliev <lopiko21@gmail.com>
2019-05-08 09:45:54 +0200

    Version 1.0

---
+++ README.md
@@ -0,0 +1,98 @@
+# Check Huawei OceanStor plugin for Nagios
+
+It serves as a hardware check of the Huawei OceanStor and was tested on OceanStor 2600V3 and OceanStor 5500v5.
+
+## PLEASE BARE IN MIND THAT YOU STILL BETTER IMPLEMENT MAIL ALARMS ON THE STORAGE ITSELF
+
+## 1. Configuration of SSH on OceanStor and nagios/monitoring server
+
+1. Create user on the Huawei OceanStor with read-only privileges.
+2. Add public ssh key through CLI to the user on OceanStor with the command: ```change user_ssh_auth_info general user_name=your_username auth_mode=publickey```
+3. It will ask for the public key, copy and paste it.
+4. Make sure that user which will execute script has the private key.
+5. Try to execute script as user which will be checking the storage system (instructions below in Usage section)
+6. Profit :)
+
+## 2. Configuration of plugin and python environment
+
+1. Plugin works with python 3 and 2.7
+2. Install python and pip
+3. You need to also install "paramiko" module with pip ```pip install paramiko```
+
+## 2. Usage
+
+**Check if the user which will be monitoring the storage system has the private key.**
+
+```bash
+usage: check-oceanstor.py [-h] -H <HOSTNAME> -u <USERNAME> -k
+                          <KEY_FILE_DESTINATION> -c
+                          <COMMAND1,COMMAND2,...,COMMANDX>
+
+Check Huawei Oceanstor through SSH
+
+    Useable commands:
+
+        lslun - show lun general status
+        lsdisk - show disk general status
+        lsdiskdomain - show disk_domain general status
+        lsexpansionmodule - show expansiom module status
+        lsinitiator - show initiator status (prints alias name for initiator)
+        lsstoragepool - show storage_pool general status
+        lsallstatuses - show all above in one check
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H <HOSTNAME>, --hostname <HOSTNAME>
+  -u <USERNAME>, --username <USERNAME>
+  -k <KEY_FILE_DESTINATION>, --key_filename <KEY_FILE_DESTINATION>
+  -c <COMMAND1,COMMAND2,...,COMMANDX>, --command <COMMAND1,COMMAND2,...,COMMANDX>
+```
+
+## 3. SAMPLE OUTPUT
+
+```text
+OK: All LUNs Online
+OK: LUN NLSAS01 status: Normal
+OK: LUN NLSAS02 status: Normal
+OK: LUN NLSAS03 status: Normal
+OK: LUN NLSAS04 status: Normal
+
+OK: All DISKs Online and Healthy
+OK: DISK CTE0.0 status: Normal
+OK: DISK CTE0.1 status: Normal
+OK: DISK CTE0.2 status: Normal
+OK: DISK CTE0.3 status: Normal
+OK: DISK CTE0.4 status: Normal
+OK: DISK CTE0.5 status: Normal
+OK: DISK CTE0.6 status: Normal
+OK: DISK CTE0.7 status: Normal
+OK: DISK CTE0.8 status: Normal
+OK: DISK DAE010.0 status: Normal
+OK: DISK DAE010.1 status: Normal
+OK: DISK DAE010.2 status: Normal
+OK: DISK DAE010.3 status: Normal
+
+OK: All DISK DOMAINs Online
+OK: DISK DOMAIN NLSASDD01 health status: Normal running status: Online
+
+OK: All EXPANSION MODULEs Online
+OK: EXPANSION MODULE DAE010.A health status: Normal running status: Running
+OK: EXPANSION MODULE DAE010.B health status: Normal running status: Running
+
+OK: All STORAGE POOLs Online
+OK: STORAGE POOL NLSAS-R6-01 health status: Normal running status: Online
+```
+
+## 4. KNOWN ISSUES
+
+1. Storage Array have active WARNING: "The number of event logs is about to reach the upper limit of 50000."\
+Details: This is because all ssh logins are put into the audit log.\
+Resolution: Add server for log dumps on OceanStor or go to Alarm Settings and mask alarm "The Space That Stores Event Logs Is To Be Used Up" in event alarms.
+
+2. Integrated Storage Manager (ISM) sends warning message "Details: The CPU usage of process *ismcli* in controller (controller enclosure CTE0, controller 0A) exceeds the threshold 50%."\
+Details: ISM CLI gets to much load on CPU when you are doing concurrent querries from nagios\
+Resolution: Monitor your OceanStor by using only one command such as -> ```python2.7 check-oceanstor.py -c lslun,lsdisk,lsinitiator``` or even ```python2.7 check-oceanstor.py -c lsallstatuses```
+
+## 5. TODO List
+
+- [ ] Empty!
---
+++ check-oceanstor.py
@@ -0,0 +1,273 @@
+import paramiko
+import sys, argparse
+from argparse import RawTextHelpFormatter
+
+if __name__ == "__main__":
+
+    # Exit code for nagios 0 -OK, 1 - Warning, 2 - Critical
+    exit_code = 0
+    output_info = ""
+    # OceanStor failed Health and Running status
+    failed_health_status = ["Offline", "Pre-fail", "Fault", "--"]
+    failed_running_status = ["Offline", "Reconstruction", "--"]
+
+    def check_empty_respone():
+        pass
+
+    def set_exit_code(code):
+        # If code is more critical than actual level set it
+        global exit_code
+        if code > exit_code:
+            exit_code = code
+
+    def lslun():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show lun general')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no LUNs defined\n"
+            
+        # Check if there are any critical LUNs
+        if not any( line.split()[4] in failed_health_status for line in lun_lines ):
+            output_info += "OK: All LUNs Online \n"
+        else:
+            output_info += "CRITICAL: check your LUN status below \n"
+            set_exit_code(2)
+
+        for line in lun_lines:
+            # Assign values
+            name, status = line.split()[1], line.split()[4]
+
+            # Check for errors
+            if status == "Normal":
+                output_info += "OK: LUN {} status: {}\n".format(name, status)
+            else:
+                output_info += "CRITICAL: LUN {} status: {}\n".format(name, status)
+                
+        return output_info
+
+    def lsdisk():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show disk general')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no DISKs defined\n"
+        
+        # Check if there are any critical DISKs
+        if not any( line.split()[1] in failed_health_status for line in lun_lines ):
+            output_info += "OK: All DISKs Online and Healthy \n"
+        else:
+            output_info += "CRITICAL: check your DISK status below \n"
+            set_exit_code(2)
+
+        for line in lun_lines:
+            # Assign values
+            slot, status, disk_type, capacity, role = line.split()[0], line.split()[1], line.split()[3], line.split()[4], line.split()[5]
+
+            # Check for errors
+            if status == "Normal":
+                output_info += "OK: DISK {} status: {}\n".format(slot, status)
+            else:
+                output_info += "CRITICAL: DISK {} status: {}\n role: {}\n type: {}\n capacity: {}\n".format(slot, status, role, disk_type, capacity)
+                
+        return output_info
+
+    def lsdiskdomain():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show disk_domain general')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no DISK DOMAINs defined\n"
+        
+        # Check if there are any critical DISK DOMAINs
+        if not any( line.split()[2] in failed_health_status for line in lun_lines ):
+            output_info += "OK: All DISK DOMAINs Online \n"
+        else:
+            output_info += "CRITICAL: check your DISK DOMAIN status \n"
+            set_exit_code(2)
+
+        for line in lun_lines:
+            # Assign values
+            name, health_status, running_status = line.split()[1], line.split()[2], line.split()[3]
+
+            # Check for errors
+            if health_status == "Normal":
+                output_info += "OK: DISK DOMAIN {} health status: {} running status: {}\n".format(name, health_status, running_status)
+            else:
+                output_info += "CRITICAL: DISK DOMAIN {} health status: {} running status: {}\n".format(name, health_status, running_status)
+                
+        return output_info
+
+    def lsexpansionmodule():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show expansion_module')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no ESXPANSIOM MODULEs defined\n"
+        
+        # Check if there are any critical EXPANSION MODULEs
+        if not any( line.split()[1] in failed_health_status for line in lun_lines ):
+            output_info += "OK: All EXPANSION MODULEs Online \n"
+        else:
+            output_info += "CRITICAL: check your EXPANSION MODULEs status \n"
+            set_exit_code(2)
+
+        for line in lun_lines:
+            # Assign values
+            expansion_id, health_status, running_status = line.split()[0], line.split()[1], line.split()[2]
+
+            # Check for errors
+            if health_status == "Normal":
+                output_info += "OK: EXPANSION MODULE {} health status: {} running status: {}\n".format(expansion_id, health_status, running_status)
+            else:
+                output_info += "CRITICAL: EXPANSION MODULE {} health status: {} running status: {}\n".format(expansion_id, health_status, running_status)
+                
+        return output_info
+
+    def lsinitiator():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show initiator')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no INITIATORs defined\n"
+        
+        # Check if there are any critical INITIATORs
+        if not any( line.split()[1] == "Offline" for line in lun_lines ):
+            output_info += "OK: All INITIATORs Online \n"
+        else:
+            output_info += "WARNING: INITIATOR OFFLINE \n"
+            set_exit_code(1)
+
+        for line in lun_lines:
+            # Assign values
+            name, status = line.split()[0], line.split()[1]
+
+            # Check for errors
+            if status == "Online":
+                output_info += "OK: INITIATOR {} status: {}\n".format(name, status)
+            else:
+                output_info += "WARNING: INITIATOR {} status: {}\n".format(name, status)
+                
+        return output_info
+
+    def lsstoragepool():
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('show storage_pool general')
+        lun_lines = ssh_stdout.readlines()[4:]
+        output_info = ""
+        
+        # return if there are no entries on storage system
+        if len(lun_lines) == 0:
+            return "OK: There are no STORAGE POOLs defined\n"
+        
+        # Check if there are any critical STORAGE POOLs
+        if not any( line.split()[3] in failed_health_status for line in lun_lines ):
+            output_info += "OK: All STORAGE POOLs Online \n"
+        else:
+            output_info += "CRITICAL: Check your STORAGE POOL status \n"
+            set_exit_code(2)
+
+        for line in lun_lines:
+            # Assign values
+            name, health_status, running_status = line.split()[1], line.split()[3], line.split()[4]
+
+            # Check for errors
+            if running_status == "Online":
+                output_info += "OK: STORAGE POOL {} health status: {} running status: {}\n".format(name, health_status, running_status)
+            else:
+                output_info += "CRITICAL: STORAGE POOL {} health status: {} running status: {}\n".format(name, health_status, running_status)
+                
+        return output_info
+
+    def lsallstatuses():
+        global output_info
+        output_info += lslun() + "\n"
+        output_info += lsdisk() + "\n"
+        output_info += lsdiskdomain() + "\n"
+        output_info += lsexpansionmodule() + "\n"
+        output_info += lsinitiator() + "\n"
+        output_info += lsstoragepool() + "\n"
+        
+        return output_info
+
+    def switcher_function(command):
+        """Function returns output information for nagios."""
+        switcher = {
+            "lslun": lslun,
+            "lsdisk": lsdisk,
+            "lsdiskdomain": lsdiskdomain,
+            "lsexpansionmodule": lsexpansionmodule,
+            "lsinitiator": lsinitiator,
+            "lsstoragepool": lsstoragepool,
+            "lsallstatuses": lsallstatuses,
+        }
+
+        return switcher.get(command)()
+    
+    help_message = ("""Check Huawei Oceanstor through SSH
+
+    Useable commands:
+
+        lslun - show lun general status
+        lsdisk - show disk general status
+        lsdiskdomain - show disk_domain general status
+        lsexpansionmodule - show expansiom module status
+        lsinitiator - show initiator status (prints alias name for initiator)
+        lsstoragepool - show storage_pool general status
+        lsallstatuses - show all above in one check""")
+    
+    useable_commands = [
+        'lslun', 
+        'lsdisk', 
+        'lsdiskdomain', 
+        'lsexpansionmodule', 
+        'lsinitiator', 
+        'lsstoragepool', 
+        'lsallstatuses'
+        ]
+
+    parser = argparse.ArgumentParser(description=help_message, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-H', '--hostname', metavar='<HOSTNAME>', required=True)
+    parser.add_argument('-u', '--username', metavar='<USERNAME>', required=True)
+    parser.add_argument('-k', '--key_filename', metavar='<KEY_FILE_DESTINATION>', required=True)
+    parser.add_argument('-c', '--command', metavar='<COMMAND1,COMMAND2,...,COMMANDX>', required=True)
+
+    args = parser.parse_args()
+
+    # Check if command exists if not throw an error
+    if not any( command in useable_commands for command in args.command.split(",") ):
+        parser.error("\n\nUnrecognized command: {0}\n\nCheck -h, --help for useable commands\n".format(args.command ) )
+        sys.exit()
+    
+    # Assign parsed arguments to variables
+    hostname = args.hostname
+    username = args.username
+    key_filename = args.key_filename
+    commands = args.command
+
+    # SSH to Oceanstor
+    ssh = paramiko.SSHClient()
+    # Accept the host_key
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect( hostname, username=username, key_filename=key_filename )
+    except:
+        error = sys.exc_info()
+        print("WARNING: Authentication failed with error: {}".format( error ))
+        sys.exit(1)
+
+    # Go to main switcher function
+    for command in commands.split(','):
+        print(switcher_function(command))
+
+    ssh.close()
+    sys.exit(exit_code)
